3ac5f46a5a3584a0404ef7c65f83b84928086bc5
WKotlinski <132941585+WKotlinski@users.noreply.github.com>
2024-02-04 13:47:27 +0100

    gallery-images

--- src/components/gallery/image-gallery.jsx
+++ src/components/gallery/image-gallery.jsx
@@ -1,33 +1,30 @@
-import { Component } from "react";
+import React from "react";
 import ImageGalleryItem from "./image-gallery-item";
 import css from "./gallery.module.css";
 import PropTypes from "prop-types";
 
-export default class ImageGallery extends Component {
-  render() {
-    const { images, onClick } = this.props;
-
-    return (
-      <ul className={css.gallery}>
-        {images.map((image) => (
-          <ImageGalleryItem
-            key={image.id}
-            image={image}
-            onClick={() => onClick(image.largeImageURL)}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+const ImageGallery = ({ images, onClick }) => {
+  return (
+    <ul className={css.gallery}>
+      {images.map((image) => (
+        <ImageGalleryItem
+          key={image.id}
+          image={image}
+          onClick={() => onClick(image.largeImageURL)}
+        />
+      ))}
+    </ul>
+  );
+};
 
 ImageGallery.propTypes = {
   images: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
       largeImageURL: PropTypes.string.isRequired,
-      // Add other properties of the 'image' object as needed
     })
   ).isRequired,
   onClick: PropTypes.func.isRequired,
 };
+
+export default ImageGallery;
